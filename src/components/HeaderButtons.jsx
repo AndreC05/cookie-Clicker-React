f7e38b042e0826aad7a75a3ff5823e896826eb4e
Andre Da Silva Carreira <andreecarreira@gmail.com>
2024-11-06 15:10:11 +0000

    Initial commit

---
+++ src/components/HeaderButtons.jsx
@@ -0,0 +1,8 @@
+export default function HeaderButtons() {
+  return (
+    <nav>
+      <button>Save</button>
+      <button>Reset</button>
+    </nav>
+  );
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import HeaderButtons from "./HeaderButtons";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Cookie Clicker: BUT NOW IN REACT!!!!</h1>
+      <HeaderButtons />
+    </header>
+  );
+}
